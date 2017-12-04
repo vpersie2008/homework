d99deba820ec3b288c492f6020af35d9dfe31dab
vpersie2008 <530689602@qq.com>
2017-12-04 21:55:29 +0800

    First vue app

---
+++ src/components/common/Header.vue
@@ -0,0 +1,16 @@
+<template>
+    <div id="header">
+    
+    </div>
+</template>
+<script>
+export default {
+  name: "header",
+
+  data() {
+    return {
+      msg: "My header"
+    };
+  }
+};
+</script>
---
+++ src/components/pages/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="home">
+      
+  </div>
+</template>
+<script>
+export default {
+  name: "home",
+  data() {
+    return {
+        homeName:"This is my frist page."
+    };
+  }
+};
+</script>
